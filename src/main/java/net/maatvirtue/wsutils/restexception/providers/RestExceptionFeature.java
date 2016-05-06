f168b87c4300ed4033588aab3175f68042796f84
Nicolas Mongrain-Lacombe <nicolas.m.lacombe@gmail.com>
2016-05-06 19:11:52 -0400

    created jaxrs feature for easy configuration

---
+++ src/main/java/net/maatvirtue/wsutils/restexception/providers/RestExceptionFeature.java
@@ -0,0 +1,26 @@
+package net.maatvirtue.wsutils.restexception.providers;
+
+import net.maatvirtue.wsutils.restexception.impl.RestExceptionScanner;
+
+import javax.ws.rs.core.Feature;
+import javax.ws.rs.core.FeatureContext;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class RestExceptionFeature implements Feature
+{
+	public RestExceptionFeature(String packageToScan)
+	{
+		new RestExceptionScanner(packageToScan).scan();
+	}
+
+	@Override
+	public boolean configure(FeatureContext context)
+	{
+		context.register(RestExceptionBodyReader.class);
+		context.register(GlobalExceptionMapper.class);
+		context.register(RestExceptionCxfClientMapper.class);
+
+		return true;
+	}
+}
